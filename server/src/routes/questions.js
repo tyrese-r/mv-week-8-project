bb60c0b97dea9494cf285a140cb0410ed5156210
Tyrese Bertie-Guadeloupe <tyrese.bertieguadeloupe@sky.uk>
2022-11-25 10:46:32 +0000

    Fixed bug where questions are not in response

--- server/src/routes/questions.js
+++ server/src/routes/questions.js
@@ -11,38 +11,15 @@ questionsRouter.get("/", async (req, res) => {
   // Check for query parameters
   if (Object.keys(req.query).includes("category")) {
     // List of category ids that are allowed
-    const allowedCategories = [
-      "9",
-      "10",
-      "11",
-      "12",
-      "13",
-      "14",
-      "15",
-      "16",
-      "17",
-      "18",
-      "19",
-      "20",
-      "21",
-      "22",
-      "23",
-      "24",
-      "25",
-      "26",
-      "27",
-      "28",
-      "29",
-      "30",
-      "31",
-      "32",
-    ];
+    const allowedCategories = [{"id":9,"name":"General Knowledge"},{"id":10,"name":"Entertainment: Books"},{"id":11,"name":"Entertainment: Film"},{"id":12,"name":"Entertainment: Music"},{"id":13,"name":"Entertainment: Musicals & Theatres"},{"id":14,"name":"Entertainment: Television"},{"id":15,"name":"Entertainment: Video Games"},{"id":16,"name":"Entertainment: Board Games"},{"id":17,"name":"Science & Nature"},{"id":18,"name":"Science: Computers"},{"id":19,"name":"Science: Mathematics"},{"id":20,"name":"Mythology"},{"id":21,"name":"Sports"},{"id":22,"name":"Geography"},{"id":23,"name":"History"},{"id":24,"name":"Politics"},{"id":25,"name":"Art"},{"id":26,"name":"Celebrities"},{"id":27,"name":"Animals"},{"id":28,"name":"Vehicles"},{"id":29,"name":"Entertainment: Comics"},{"id":30,"name":"Science: Gadgets"},{"id":31,"name":"Entertainment: Japanese Anime & Manga"},{"id":32,"name":"Entertainment: Cartoon & Animations"}]
 
     // Check categories are valid
-    const isValidCategory = allowedCategories.includes(req.query.category);
+    const isValidCategory = allowedCategories.map(c => c.name).includes(req.query.category);
+    console.log(req.query.category)
+
 
     if (isValidCategory) {
-      chosenCategory = req.query.category;
+      chosenCategory = allowedCategories.find(c => c.name == req.query.category).name;
     } else {
       res.statusCode = 400;
       res.send({ message: "Invalid category" });
@@ -71,13 +48,13 @@ questionsRouter.get("/", async (req, res) => {
   res.send({
     chosen_category: chosenCategory,
     chosen_difficulty: chosenDifficulty,
-    count: Questions.count({
+    count: await Questions.count({
       where: {
         category: chosenCategory,
         difficulty: chosenDifficulty,
       },
     }),
-    question: Questions.findAll({
+    question: await Questions.findAll({
       where: {
         category: chosenCategory,
         difficulty: chosenDifficulty,
